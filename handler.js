04d1dfbf8160da26b44a81c2027c2de6ef878458
Javier Quinteros <javier.quinteros@inacapmail.cl>
2021-03-24 16:34:38 -0300

    Gitrow added

--- handler.js
+++ handler.js
@@ -14,6 +14,30 @@ app.get("/hello", (req, res, next) => {
   });
 });
 
+app.get("/bombones", (req, res, next) => {
+  // If you use GitRows as a module:
+  const Gitrows = require('gitrows');
+
+  // Init the GitRows client, you can provide options at this point, later or just run on the defaults
+  const gitrows = new Gitrows();
+
+  let path = 'https://github.com/NORA-CO/Datos-COVID19/blob/master/output/producto74/paso_a_paso.csv';
+
+  gitrows.get(path)
+    .then((data) => {
+      //handle (Array/Object)data
+      //console.log(data);
+      //console.log(data);
+      return res.status(200).json({
+        message: data,
+      });
+
+    })
+
+
+});
+
+
 app.use((req, res, next) => {
   return res.status(404).json({
     error: "Not Found",
