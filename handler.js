3873d928a5089581438c0bd3b29ddb85a1f8f4ae
camilo <kevinmellav@gmail.com>
2021-04-12 21:47:18 -0400

    prueba iso

--- handler.js
+++ handler.js
@@ -242,9 +242,14 @@ app.get("/bombones/resumenComuna", (req, res, next) => {
       }).catch( (error) => {
         resta = resta - 1;
       });
-  }while(resta > -4)
+  }while(resta < -4)
   
   objectArray.casosActivos = resta;
+  var fechaArchivo2 = new Date();
+  fechaArchivo2.setDate(currentDate.getDate() + resta);
+  var fechaArchivoISO2 = fechaArchivo.toISOString().slice(0, 10);
+  objectArray.comuna = path.replace(fechaISO,fechaArchivoISO2);
+  objectArray.fecha = fechaArchivoISO2;
 
   return res.status(200).json({
     message: objectArray,
